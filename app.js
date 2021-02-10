2fbaf82cc7fa342a73c194c9ca3b751718679f6d
gitrocket <emilewamsteker@gmail.com>
2021-02-10 15:58:33 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+
+function modalOpen() {
+    modalOverlay.classList.add("open-modal")
+}
+
+function modalClose() {
+    modalOverlay.classList.remove("open-modal");
+}
+
+
+modalBtn.addEventListener('click', modalOpen);
+
+closeBtn.addEventListener('click', modalClose);
